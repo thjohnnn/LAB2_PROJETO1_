b805ab084ffd4968cd592b37399a9bf6c1285677
João Pedro <joaopedroleitaog59@gmail.com>
2025-08-13 23:39:46 -0300

    sorteio de 7 números com cálculo da média

--- principal.py
+++ principal.py
@@ -1,4 +1,12 @@
-def cumprimento(texto: str) -> str:
-    return f"Olá, {texto}"
+import random
+from statistics import mean
+
+def sorteia_sete_e_media():
+    numeros = [random.randint(0, 100) for _ in range(7)] 
+    return numeros, mean(numeros)
+
 
 print(cumprimento("João Pedro Paravidino"))
+nums, media = sorteia_sete_e_media()
+print("Números sorteados:", nums)
+print("Média:", round(media, 2))
